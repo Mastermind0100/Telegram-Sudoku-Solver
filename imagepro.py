c0eef41bcf2bb33c299cf0f085017b0637e933a7
Atharva <atharvahudlikar@gmail.com>
2020-03-24 16:01:03 +0530

    Minor Changes

--- imagepro.py
+++ imagepro.py
@@ -9,9 +9,6 @@ from keras.utils import to_categorical
 import operator
 import os
 import platform
-import pytesseract
-
-# pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 arr_out = []
 arr_result = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
